485e3f71b66427cc8562d9bbc87ce902aa312c5e
Indi Tenorio <indilitenorio@gmail.com>
2024-02-08 17:59:31 -0300

    Preparando para deploy

--- knexfile.ts
+++ knexfile.ts
@@ -4,10 +4,13 @@ import { env } from "./src/env";
 // Update with your config settings.
 
 const config: Knex.Config = {
-  client: "sqlite3",
-  connection: {
-    filename: env.DATABASE_URL,
-  },
+  client: env.DATABASE_CLIENT,
+  connection:
+    env.DATABASE_CLIENT === "sqlite"
+      ? {
+          filename: env.DATABASE_URL,
+        }
+      : env.DATABASE_URL,
 
   migrations: {
     directory: "db/migrations",
