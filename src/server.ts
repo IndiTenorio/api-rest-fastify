fb4027c280a3ef55af992985e159a9e9bee772a2
Indi Tenorio <indilitenorio@gmail.com>
2024-02-08 19:46:01 -0300

    Testando host

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { server } from "./app";
 import { env } from "./env";
 
-server.listen({ port: env.PORT }, () => {
+server.listen({ port: env.PORT, host: "0.0.0.0" }, () => {
   console.log(`listening at http://localhost:${env.PORT}`);
 });
